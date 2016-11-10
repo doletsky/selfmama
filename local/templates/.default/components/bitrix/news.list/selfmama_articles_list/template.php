d3f71e57462d06993e73e042d869b3df8bc86dce
adoletskiy <adoletsky@indigos.ru>
2016-11-10 07:59:10 +0300

    articals page mobil - debug view first article

--- local/templates/.default/components/bitrix/news.list/selfmama_articles_list/template.php
+++ local/templates/.default/components/bitrix/news.list/selfmama_articles_list/template.php
@@ -32,7 +32,7 @@ global $APPLICATION;
         $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
         $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
         $arCategory = $arResult['CATEGORIES'][$arItem['CAT_ID']]?>
-                <a  href="<?=$arItem['DETAIL_PAGE_URL']?>" <?=$arItem['TARGET']?> style="background-image: url('<?=$arItem['DETAIL_PICTURE']['SRC']?>')" class="article <?=$arItem['CLASS']?>">
+                <a id="<?if($k==0) echo 'f_article';?>"  href="<?=$arItem['DETAIL_PAGE_URL']?>" <?=$arItem['TARGET']?> style="background-image: url('<?=$arItem['DETAIL_PICTURE']['SRC']?>')" class="article <?=$arItem['CLASS']?>">
                     <span class="overlay"></span>
                         <span id="<?=$this->GetEditAreaId($arItem['ID']);?>" class="info">
                             <span class="ins">
